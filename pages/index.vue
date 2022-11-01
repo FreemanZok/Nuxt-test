1468168a8be20a1ff10b6941d0e70f6ae02222d9
ali zokaei <ali.zokaei.1367@gmail.com>
2022-11-01 16:30:55 +0330

    hi hello

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,18 @@
 <template>
   <div>
     <test2 />
-    <test3 />
+    <test4 />
   </div>
 </template>
 
 <script>
 export default {
+  created(){
+    // let myBody = document.getElementsByTagName("BODY")[0];
+    // myBody.classList.add("dx-viewport")
+  },
+  mounted(){
 
+  }
 }
 </script>
