116a3d15abc3017104a0dd4f46852c18d1e4e542
ali zokaei <ali.zokaei.1367@gmail.com>
2022-11-01 12:43:37 +0330

    test succec devextrime

---
+++ components/test.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <DxButton
+        text="Click me"
+        @click="sayHelloWorld"
+    />
+  </div>
+</template>
+
+<script>
+import DxButton from 'devextreme-vue/button';
+export default {
+    components: {
+        DxButton
+    },
+    methods: {
+        sayHelloWorld() {
+            alert('Hello world!')
+        }
+    }
+}
+</script>
