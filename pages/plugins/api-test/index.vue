7f31fd54af4a80af86ee3143a8b3a497e249d4ad
ali zokaei <ali.zokaei.1367@gmail.com>
2022-11-03 01:06:00 +0330

    hi hello

---
+++ pages/plugins/api-test/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="text-center pt-4 px-4">
+
+    <button @click="getApi()" class="btn btn-success w-100">click me to get api test</button>
+  </div>
+</template>
+<script>
+export default {
+  components: {
+
+  },
+  data() {
+    return {
+      hello: "hello world"
+    }
+  },
+  methods: {
+    async getApi() {
+      const get_data = await this.$axios.$get('https://randomuser.me/api?results=1')
+      console.log(get_data)
+      alert("result in consol")
+    }
+  }
+}
+</script>
